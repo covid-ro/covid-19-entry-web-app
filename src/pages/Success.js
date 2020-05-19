0df19ac45f44eb289e04408e2f669f72592b3b61
Cezar Neaga <hi@cezarneaga.eu>
2020-05-19 22:22:25 +0200

    add qr code

--- src/pages/Success.js
+++ src/pages/Success.js
@@ -1,23 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Box, Heading, Button, Text } from '@chakra-ui/core'
+import { Box, Flex, Heading, Button, Text } from '@chakra-ui/core'
+import { QRCode } from 'react-qr-svg'
 import { Trans } from '../locale/Trans'
 import { WhiteBox } from '../components/WhiteBox'
 export function Success() {
   const declarationCodes = JSON.parse(localStorage.getItem('declaration_code'))
   return (
-    <WhiteBox>
-      <Box
-        mt="4"
-        mb="16"
-        d="flex"
-        flexDirection="column"
-        alignItems="center"
-        justifyContent="center">
-        <Text>
-          <Trans id="yourCodesLabel" />
-        </Text>
-        {declarationCodes.map((code) => (
+    <WhiteBox p={[1, 8]}>
+      <Text textAlign="center">
+        <Trans id="yourCodesLabel" />
+      </Text>
+      {declarationCodes.map((code) => (
+        <Flex
+          mt="4"
+          mb="16"
+          d="flex"
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="center">
           <Button
             variantColor="brand"
             variant="outline"
@@ -28,8 +29,15 @@ export function Success() {
             fontWeight="bold">
             {code}
           </Button>
-        ))}
-      </Box>
+          <QRCode
+            bgColor="#FFFFFF"
+            fgColor="#000000"
+            level="Q"
+            style={{ width: 256 }}
+            value={code.toString()}
+          />
+        </Flex>
+      ))}
       <Heading size="md" lineHeight="32px" fontWeight="bold">
         <Trans id="finishScreenFirstLine" />
       </Heading>
