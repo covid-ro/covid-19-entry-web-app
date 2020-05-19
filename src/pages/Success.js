04f89e6fc81a500951d0291c3cae8b4e2ce3bce8
Cezar Neaga <hi@cezarneaga.eu>
2020-05-19 22:49:46 +0200

    add modal for qr

--- src/pages/Success.js
+++ src/pages/Success.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Box, Flex, Heading, Button, Text } from '@chakra-ui/core'
+import {
+  Box,
+  Flex,
+  Heading,
+  Button,
+  Text,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+} from '@chakra-ui/core'
+
 import { QRCode } from 'react-qr-svg'
 import { Trans } from '../locale/Trans'
 import { WhiteBox } from '../components/WhiteBox'
 export function Success() {
+  const { isOpen, onOpen, onClose } = useDisclosure()
   const declarationCodes = JSON.parse(localStorage.getItem('declaration_code'))
   return (
     <WhiteBox p={[1, 8]}>
@@ -18,7 +34,9 @@ export function Success() {
           d="flex"
           flexDirection="column"
           alignItems="center"
-          justifyContent="center">
+          justifyContent="center"
+          onClick={onOpen}
+          style={{ cursor: 'pointer' }}>
           <Button
             variantColor="brand"
             variant="outline"
@@ -36,6 +54,28 @@ export function Success() {
             style={{ width: 256 }}
             value={code.toString()}
           />
+          <Modal isOpen={isOpen} onClose={onClose} isCentered size="full">
+            <ModalOverlay backgroundColor={'rgba(255,255,255,1)'} />
+            <ModalContent>
+              <ModalHeader>Nume Prenume</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody d="flex" flexDirection="column" alignItems="center">
+                <QRCode
+                  bgColor="#FFFFFF"
+                  fgColor="#000000"
+                  level="Q"
+                  style={{ width: 300 }}
+                  value={code.toString()}
+                />
+              </ModalBody>
+
+              <ModalFooter>
+                <Button variantColor="brand" mr={3} onClick={onClose}>
+                  Închide
+                </Button>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
         </Flex>
       ))}
       <Heading size="md" lineHeight="32px" fontWeight="bold">
