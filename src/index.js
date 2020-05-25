e89a52ae19b2733b728c6731742af6b5f363d765
Cezar Neaga <hi@cezarneaga.eu>
2020-05-25 21:00:08 +0200

    add sentry logging with envs

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import * as Sentry from '@sentry/browser'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { ThemeProvider, CSSReset } from '@chakra-ui/core'
 import { customTheme } from './theme'
 import { LanguageProvider } from './locale/LanguageContext'
 
 import App from './App'
+Sentry.init({
+  dsn:
+    'https://4b39d10c7bb24d43b0fd04e9417e7138@o395451.ingest.sentry.io/5253033',
+  environment: process.env.NODE_ENV,
+})
 
 ReactDOM.render(
-  // <React.StrictMode>
   <ThemeProvider theme={customTheme}>
     <LanguageProvider>
       <CSSReset />
