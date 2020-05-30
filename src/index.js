541877a6a2d1cb86e6dd60ea3f78b7550ed8421a
Cezar Neaga <hi@cezarneaga.eu>
2020-05-30 17:37:49 +0200

    closed #21 - scroll top

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import { customTheme } from './theme'
 import { LanguageProvider } from './locale/LanguageContext'
 
 import App from './App'
+import ScrollToTop from './components/ScrollToTop'
 Sentry.init({
   dsn:
     'https://4b39d10c7bb24d43b0fd04e9417e7138@o395451.ingest.sentry.io/5253033',
@@ -47,6 +48,7 @@ ReactDOM.render(
       </Helmet>
       <CSSReset />
       <Router>
+        <ScrollToTop />
         <App />
       </Router>
     </LanguageProvider>
