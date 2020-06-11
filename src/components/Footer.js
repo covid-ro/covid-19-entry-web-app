6b701f30fe7f2fa09558dc431a990abe591649c7
Cezar Neaga <hi@cezarneaga.eu>
2020-06-11 19:02:21 +0200

    add logos in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,8 @@
 import React from 'react'
-import { Box, Link, Flex } from '@chakra-ui/core'
+import { Box, Link, Flex, Image, Stack } from '@chakra-ui/core'
 import { Trans } from '../locale/Trans'
+import cn from '../assets/images/cn.svg'
+import sts from '../assets/images/logo.png'
 export function Footer() {
   return (
     <Flex width="100%" maxW="760px" mx="auto" px={[2, 4, 10]}>
@@ -21,6 +23,26 @@ export function Footer() {
         <Link isExternal href="https://sts.ro" color="brand.500">
           <Trans id="sts" />
         </Link>
+        <Stack isInline alignItems="center" justifyContent="center" mt="2">
+          <Link isExternal href="https://citizennext.ro">
+            <Image
+              size="40px"
+              objectFit="cover"
+              src={cn}
+              alt="Citizen Next"
+              mx="4"
+            />
+          </Link>
+          <Link isExternal href="https://sts.ro">
+            <Image
+              size="30px"
+              objectFit="center"
+              src={sts}
+              alt={<Trans id="sts" />}
+              mx="4"
+            />
+          </Link>
+        </Stack>
       </Box>
     </Flex>
   )
