7bcb12187a4ced50ed598b90d898a773016cee49
Cezar Neaga <hi@cezarneaga.eu>
2020-06-21 15:32:46 +0200

    add social sharing

--- src/index.js
+++ src/index.js
@@ -19,8 +19,8 @@ ReactDOM.render(
   <ThemeProvider theme={customTheme}>
     <ColorModeProvider>
       <LanguageProvider>
-        <Helmet titleTemplate={`%s | Covid-SAFE@Frontiera`}>
-          <meta name="description" content="Covid-SAFE@Frontiera" />
+        <Helmet titleTemplate={`%s | Covid-SAFE@Frontieră`}>
+          <meta name="description" content="Covid-SAFE@Frontieră" />
           <meta
             name="robots"
             content="max-snippet:-1, max-image-preview:large, max-video-preview:-1"
@@ -28,24 +28,36 @@ ReactDOM.render(
           <link rel="canonical" href={process.env.PUBLIC_URL} />
           <meta property="og:locale" content="ro_RO" />
           <meta property="og:type" content="website" />
-          <meta property="og:title" content="Covid-SAFE@Frontiera" />
+          <meta property="og:title" content="Covid-SAFE@Frontieră" />
           <meta
             property="og:description"
             content="Această aplicație este pusă la dispoziție de Guvernul României și stă la baza declarației de intrare în țară la punctele vamale, urmare a pandemiei de coronavirus COVID-19."
           />
           <meta property="og:url" content={process.env.PUBLIC_URL} />
-          <meta property="og:site_name" content="Covid-SAFE@Frontiera" />
-          <meta property="og:image" content="logo192.png" />
-          <meta property="og:image:secure_url" content="logo192.png" />
+          <meta property="og:site_name" content="Covid-SAFE@Frontieră" />
+          <meta
+            property="og:image"
+            content="https://cdn.citizennext.ro/projects/covid-safe-frontiera/covid-safe-frontiera-banner-1.png"
+          />
+          <meta
+            property="og:image:secure_url"
+            content="https://cdn.citizennext.ro/projects/covid-safe-frontiera/covid-safe-frontiera-banner-1.png"
+          />
           <meta property="og:image:width" content="1280" />
           <meta property="og:image:height" content="720" />
-          <meta name="twitter:card" content="logo192.png" />
+          <meta
+            name="twitter:card"
+            content="https://cdn.citizennext.ro/projects/covid-safe-frontiera/covid-safe-frontiera-banner-1.png"
+          />
           <meta
             name="twitter:description"
             content="Această aplicație este pusă la dispoziție de Guvernul României și stă la baza declarației de intrare în țară la punctele vamale, urmare a pandemiei de coronavirus COVID-19."
           />
-          <meta name="twitter:title" content="Covid-SAFE@Frontiera  " />
-          <meta name="twitter:image" content="logo192.png" />
+          <meta name="twitter:title" content="Covid-SAFE@Frontieră  " />
+          <meta
+            name="twitter:image"
+            content="https://cdn.citizennext.ro/projects/covid-safe-frontiera/covid-safe-frontiera-banner-1.png"
+          />
         </Helmet>
         <CSSReset />
         <Router>
